fcf7d055d5b18a266add06888f83054d7ba6e73b
mateussilva84729156-lab <mateus.silva84729156@gmail.com>
2026-08-12 09:59:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -291,7 +291,7 @@ function goToCheckout() {
   // Facebook Pixel Event: InitiateCheckout
   if (typeof fbq !== 'undefined') fbq('track', 'InitiateCheckout');
   
-  window.location.href = 'https://smb4.ofertas.my/pay-v2/62de8de1-9ade-4e03-95ec-8799c8594bf6';
+  window.location.href = 'https://stf4.ofertas.my/pay-v2/8e9b6232-0fc1-4e52-9cf6-521836ded7c4';
 }
 
 /* ---- Checkout Countdown ---- */
